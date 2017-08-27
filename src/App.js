32f7dc1f5c2913e386aacb6712f7ea5336f479f5
Luke Hamilton <lukehamiltonmail@gmail.com>
2017-08-27 22:26:28 +0300

    Launching meta mask from browser - WIP

--- src/App.js
+++ src/App.js
@@ -2,52 +2,67 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import { HiddenOnlyAuth, VisibleOnlyAuth } from './util/wrappers.js'
 
-// UI Components
-import LoginButtonContainer from './user/ui/loginbutton/LoginButtonContainer'
-import LogoutButtonContainer from './user/ui/logoutbutton/LogoutButtonContainer'
+// Import libraries we need.
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
 
 // Styles
 import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
+// import abi from '../build/contracts/NonsenseTokenCrowdsale.json'
+import artifacts from '../build/contracts/NonsenseTokenCrowdsale.json'
+let Crowdsale = contract(artifacts)
+// let crowdsaleContractInstance = CrowdsaleContract.at(address)
+// var NonsenseTokenCrowdsale = contract(nonsense_token_crowdsale_artifacts)
 
 class App extends Component {
-  render() {
-    const OnlyAuthLinks = VisibleOnlyAuth(() =>
-      <span>
-        <li className="pure-menu-item">
-          <Link to="/dashboard" className="pure-menu-link">Dashboard</Link>
-        </li>
-        <li className="pure-menu-item">
-          <Link to="/profile" className="pure-menu-link">Profile</Link>
-        </li>
-        <LogoutButtonContainer />
-      </span>
-    )
 
-    const OnlyGuestLinks = HiddenOnlyAuth(() =>
-      <span>
-        <li className="pure-menu-item">
-          <Link to="/signup" className="pure-menu-link">Sign Up</Link>
-        </li>
-        <LoginButtonContainer />
-      </span>
-    )
+  render() {
 
     return (
       <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-          <Link to="/" className="pure-menu-heading pure-menu-link">Truffle Box</Link>
-          <ul className="pure-menu-list navbar-right">
-            <OnlyGuestLinks />
-            <OnlyAuthLinks />
-          </ul>
-        </nav>
-
-        {this.props.children}
+        <a onClick={this.sendTx}>Send TX</a>
       </div>
     );
+  }
+
+  sendTx()  {
+    // console.log('ABI', abi);
+    // var abi=[//your abi array];
+    let web3 = window.web3
+    let account1 = web3.eth.accounts[1]
+    Crowdsale.setProvider(web3.currentProvider)
+    Crowdsale.deployed().then(function(instance)  {
+      instance.sendTransaction({ from: account1, value: web3.toWei(5, "ether")})
+    })
+
+    // var contractAddress = "0x36de1fcd6c23c8b66ff14105b84cbd7674863dd6";
+    // var contractAddress = '0x6b5decb6b58c88c70844587169d8e6e471a7925a'
+    // var contract = web3.eth.contract(abi.abi).at(contractAddress);
+    // console.log('contract', contract);
+    // contract.sendTransaction({ from: account1, value: web3.toWei(5, "ether")})
+    // let address = '0x36de1fcd6c23c8b66ff14105b84cbd7674863dd6'
+    // let CrowdsaleContract = contract(abi)
+    // let crowdsaleContractInstance = CrowdsaleContract.at(address)
+    //
+    // miniTokentoken.transfer(toAddress, value, { from: addr })
+    // .then(function (txHash) {
+    //   console.log('Transaction sent')
+    //   console.dir(txHash)
+    //   waitForTxToBeMined(txHash)
+    // })
+    // .catch(console.error)
+
+    // Web3
+    // let web3 = window.web3
+    // let account1 = web3.eth.accounts[1]
+    // web3.eth.contract('0x36de1fcd6c23c8b66ff14105b84cbd7674863dd6').sendTransaction({ from: account1, value: web3.toWei(5, "ether")})
+    // crowdsaleContractInstance.sendTransaction({ from: account1, value: web3.toWei(5, "ether")})
+    // console.log('web3', window.web3.currentProvider);
+    console.log('SENDinG TX');
+    // console.log(nonsense_token_crowdsale_artifacts);
   }
 }
 
